0bcc7f976a1bcb4911289394ca5660946610b735
Sam Reaves <thesamreaves@gmail.com>
2013-10-08 04:28:33 -0400

    Columns loosely formed and pieces will retain their boundaries if treated carefully. Need to tighten up the columns by fixing the left variable. Perhaps I should've used floats instead of absolutes? Spent way too much time on whiteboarding the AI (because it was a challenge I knew I could overcome) but didn't focus enough on the front end, which is my specialty. I will fix this tomorrow. It's 4:30am on Tuesday morning.

--- connectfour.js
+++ connectfour.js
@@ -55,37 +55,219 @@ function dropToken(token, space) {
 
 var gameboard = [42];
 var gamecount = 1;
-function addPiece(token) {
-    if (gameboard[6] === undefined) {
-        gameboard[6] = 8;
-        dropToken(token, 6);
-        gamecount++;
+function defineColumn(tokenPosition) {
+    if (tokenPosition >= 431 && tokenPosition < 514) {
+        return 1;
     }
-    else if (gameboard[5] === undefined) {
-        gameboard[5] = 8;
-        dropToken(token, 5);
-        gamecount++;
+    if (tokenPosition >= 514 && tokenPosition < 597) {
+        return 2;
     }
-    else if (gameboard[4] === undefined) {
-        gameboard[4] = 8;
-        dropToken(token, 4);
-        gamecount++;
+    if (tokenPosition >= 597 && tokenPosition < 680) {
+        return 3;
     }
-    else if (gameboard[3] === undefined) {
-        gameboard[3] = 8;
-        dropToken(token, 3);
-        gamecount++;
+    if (tokenPosition >= 680 && tokenPosition < 763) {
+        return 4;
     }
-    else if (gameboard[2] === undefined) {
-        gameboard[2] = 8;
-        dropToken(token, 2);
-        gamecount++;
+    if (tokenPosition >= 763 && tokenPosition < 846) {
+        return 5;
     }
-    else if (gameboard[1] === undefined) {
-        gameboard[1] = 8;
-        dropToken(token, 1);
-        gamecount++;
+    if (tokenPosition >= 846 && tokenPosition < 929) {
+        return 6;
     }
+    if (tokenPosition >= 929) {
+        return 7;
+    }
+}
+function addPiece(token, column, player) {
+    if (player === true) {
+        player = 8;
+    }
+    else {
+        player = 9;
+    }
+    if (column === 1) {
+        if (gameboard[6] === undefined) {
+            gameboard[6] = player;
+            dropToken(token, 6);
+        }
+        else if (gameboard[5] === undefined) {
+            gameboard[5] = player;
+            dropToken(token, 5);
+        }
+        else if (gameboard[4] === undefined) {
+            gameboard[4] = player;
+            dropToken(token, 4);
+        }
+        else if (gameboard[3] === undefined) {
+            gameboard[3] = player;
+            dropToken(token, 3);
+        }
+        else if (gameboard[2] === undefined) {
+            gameboard[2] = player;
+            dropToken(token, 2);
+        }
+        else if (gameboard[1] === undefined) {
+            gameboard[1] = player;
+            dropToken(token, 1);
+        }
+    }
+    else if (column === 2) {
+         if (gameboard[12] === undefined) {
+            gameboard[12] = player;
+            dropToken(token, 6);
+        }
+        else if (gameboard[11] === undefined) {
+            gameboard[11] = player;
+            dropToken(token, 5);
+        }
+        else if (gameboard[10] === undefined) {
+            gameboard[10] = player;
+            dropToken(token, 4);
+        }
+        else if (gameboard[9] === undefined) {
+            gameboard[9] = player;
+            dropToken(token, 3);
+        }
+        else if (gameboard[8] === undefined) {
+            gameboard[8] = player;
+            dropToken(token, 2);
+        }
+        else if (gameboard[7] === undefined) {
+            gameboard[7] = player;
+            dropToken(token, 1);
+        }       
+    }
+    else if (column === 3) {
+          if (gameboard[18] === undefined) {
+            gameboard[18] = player;
+            dropToken(token, 6);
+        }
+        else if (gameboard[17] === undefined) {
+            gameboard[17] = player;
+            dropToken(token, 5);
+        }
+        else if (gameboard[16] === undefined) {
+            gameboard[16] = player;
+            dropToken(token, 4);
+        }
+        else if (gameboard[15] === undefined) {
+            gameboard[15] = player;
+            dropToken(token, 3);
+        }
+        else if (gameboard[14] === undefined) {
+            gameboard[14] = player;
+            dropToken(token, 2);
+        }
+        else if (gameboard[13] === undefined) {
+            gameboard[13] = player;
+            dropToken(token, 1);
+        }           
+    }
+    else if (column === 4) {
+         if (gameboard[23] === undefined) {
+            gameboard[23] = player;
+            dropToken(token, 6);
+        }
+        else if (gameboard[22] === undefined) {
+            gameboard[22] = player;
+            dropToken(token, 5);
+        }
+        else if (gameboard[21] === undefined) {
+            gameboard[21] = player;
+            dropToken(token, 4);
+        }
+        else if (gameboard[20] === undefined) {
+            gameboard[20] = player;
+            dropToken(token, 3);
+        }
+        else if (gameboard[20] === undefined) {
+            gameboard[20] = player;
+            dropToken(token, 2);
+        }
+        else if (gameboard[19] === undefined) {
+            gameboard[19] = player;
+            dropToken(token, 1);
+        }            
+    }
+    else if (column === 5) {
+          if (gameboard[30] === undefined) {
+            gameboard[30] = player;
+            dropToken(token, 6);
+        }
+        else if (gameboard[29] === undefined) {
+            gameboard[29] = player;
+            dropToken(token, 5);
+        }
+        else if (gameboard[28] === undefined) {
+            gameboard[28] = player;
+            dropToken(token, 4);
+        }
+        else if (gameboard[27] === undefined) {
+            gameboard[27] = player;
+            dropToken(token, 3);
+        }
+        else if (gameboard[26] === undefined) {
+            gameboard[26] = player;
+            dropToken(token, 2);
+        }
+        else if (gameboard[25] === undefined) {
+            gameboard[25] = player;
+            dropToken(token, 1);
+        }           
+    }
+    else if (column === 6) {
+         if (gameboard[36] === undefined) {
+            gameboard[36] = player;
+            dropToken(token, 6);
+        }
+        else if (gameboard[35] === undefined) {
+            gameboard[35] = player;
+            dropToken(token, 5);
+        }
+        else if (gameboard[34] === undefined) {
+            gameboard[34] = player;
+            dropToken(token, 4);
+        }
+        else if (gameboard[33] === undefined) {
+            gameboard[33] = player;
+            dropToken(token, 3);
+        }
+        else if (gameboard[32] === undefined) {
+            gameboard[32] = player;
+            dropToken(token, 2);
+        }
+        else if (gameboard[31] === undefined) {
+            gameboard[31] = player;
+            dropToken(token, 1);
+        }            
+    }
+    else if (column === 7) {
+         if (gameboard[42] === undefined) {
+            gameboard[42] = player;
+            dropToken(token, 6);
+        }
+        else if (gameboard[41] === undefined) {
+            gameboard[41] = player;
+            dropToken(token, 5);
+        }
+        else if (gameboard[40] === undefined) {
+            gameboard[40] = player;
+            dropToken(token, 4);
+        }
+        else if (gameboard[39] === undefined) {
+            gameboard[39] = player;
+            dropToken(token, 3);
+        }
+        else if (gameboard[38] === undefined) {
+            gameboard[38] = player;
+            dropToken(token, 2);
+        }
+        else if (gameboard[37] === undefined) {
+            gameboard[37] = player;
+            dropToken(token, 1);
+        }            
+    }
+    gamecount++;
     if (gamecount % 2 === 1) {
         $("#drop_space").append("<div class=\"token player1 currenttoken\" id=\"token" + gamecount + "\"></div>");
         $("#messages").text("Player 1: Drop your piece below!");
@@ -100,10 +282,13 @@ function addPiece(token) {
 
 $(document).ready(function() {
     $("#drop_space").on('click', '.token', function(e) {
-        addPiece(this);
+        var element = document.getElementsByClassName("currenttoken")[0];
+        var column = element.getBoundingClientRect();
+        addPiece(this, defineColumn(column.left), $(".currenttoken").hasClass("player1"));
     });
+    var currentMousePos;
     $("div").mousemove(function(e) {
-        var currentMousePos = e.pageX - $('#drop_space').offset().left - 38;
+        currentMousePos = e.pageX - $('#drop_space').offset().left - 38;
         $(".currenttoken").css("left", currentMousePos);
     });
 
